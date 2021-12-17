5f01f20ca2ce12b05c3ffbe13daa93f9cc7ac861
Sahia123@gmail.com <sahiatech123@gmail.com>
2021-12-17 10:34:33 +0500

    This is my 1st commit

---
+++ WebFormApplication/WebForm1.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebFormApplication.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <center>
+    <form id="form1" runat="server">
+        <div>
+          ID:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+        </div>
+        <p>
+          Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+        </p>
+        <p>
+          Phone:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox>
+        </p>
+        <p>
+          Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+        </p>
+        <p>
+         Password:&nbsp;&nbsp;&nbsp;   <asp:TextBox ID="txtPassword" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="btn_Submit" runat="server" Text="Submit" OnClick="btn_Submit_Click" Width="122px" />
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <asp:GridView ID="GridView1" AutoGenerateColumns="false" runat="server" BackColor="#CCCCCC" BorderColor="#999999" BorderStyle="Solid" BorderWidth="3px" CellPadding="4" CellSpacing="2" ForeColor="Black" Width="500px">
+            <Columns>
+                <asp:BoundField HeaderText="ID" DataField="Id" />
+                <asp:BoundField HeaderText="Name" DataField="Name" />
+                <asp:BoundField HeaderText="Phone" DataField="Phone" />
+                <asp:BoundField HeaderText="Email" DataField="Email" />
+                <asp:BoundField HeaderText="Password" DataField="Password" />
+                <asp:TemplateField HeaderText="Delete">
+                    <ItemTemplate>
+                        <asp:ImageButton ID="ImageButton1" runat="server" ImageUrl="~/Images/dlt.jpg" Width="30px" OnClick="ImageButton1_Click" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Edit">
+                    <ItemTemplate>
+                        <asp:ImageButton ID="Btn_Edit" runat="server" ImageUrl="~/Images/edit.png" Width="30px" OnClick="Btn_Edit_Click" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+            <FooterStyle BackColor="#CCCCCC" />
+            <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#CCCCCC" ForeColor="Black" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" />
+            <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#808080" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#383838" />
+        </asp:GridView>
+    </form></center>
+</body>
+</html>
---
+++ WebFormApplication/WebForm1.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebFormApplication.ServiceReference1;
+
+namespace WebFormApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        Service1Client service = new Service1Client();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GridView1.DataSource = null;
+                GridView1.DataSource = service.getStudents();
+                GridView1.DataBind();
+            }
+        }
+
+        protected void btn_Submit_Click(object sender, EventArgs e)
+        {
+            int id = int.Parse(txtID.Text);
+            string name = txtName.Text;
+            string phone = txtPhone.Text;
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            if (btn_Submit.Text == "Submit")
+            {
+                bool isInsert = service.insert(name, phone, email, password);
+                if (isInsert)
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "callfunction", "alert('Save Successfully')", true);
+                    GridView1.DataSource = null;
+                    GridView1.DataSource = service.getStudents();
+                    GridView1.DataBind();
+                    txtID.Text =  "";
+                    txtName.Text = "";
+                    txtPhone.Text = "";
+                    txtEmail.Text = "";
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "callfunction", "alert('Error')", true);
+                }
+            }
+            else if(btn_Submit.Text=="Save Changes")
+            {
+                bool isUpdate = service.update(id,name, phone, email, password);
+                if (isUpdate)
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "callfunction", "alert('Updated Successfully')", true);
+                    GridView1.DataSource = null;
+                    GridView1.DataSource = service.getStudents();
+                    GridView1.DataBind();
+                    txtID.Text = "";
+                    txtName.Text = "";
+                    txtPhone.Text = "";
+                    txtEmail.Text = "";
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "callfunction", "alert('Error')", true);
+                }
+            }
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            ImageButton btn = sender as ImageButton;
+            GridViewRow row = btn.NamingContainer as GridViewRow;
+            int id = int.Parse(row.Cells[0].Text);
+            bool isDelete = service.delete(id);
+            if (isDelete)
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "callfunction", "alert('Deleted Successfully')", true);
+                GridView1.DataSource = null;
+                GridView1.DataSource = service.getStudents();
+                GridView1.DataBind();
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "callfunction", "alert('Error')", true);
+            }
+        }
+
+        protected void Btn_Edit_Click(object sender, ImageClickEventArgs e)
+        {
+            ImageButton btn = sender as ImageButton;
+            GridViewRow row = btn.NamingContainer as GridViewRow;
+            int id = int.Parse(row.Cells[0].Text);
+            Student obj = service.getSingleRecord(id);
+            txtID.Text = obj.Id + "";
+            txtName.Text = obj.Name;
+            txtPhone.Text = obj.Phone;
+            txtEmail.Text = obj.Email;
+            txtPassword.Text = obj.Password;
+            btn_Submit.Text = "Save Changes";
+        }
+    }
+}
